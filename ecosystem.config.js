6b20625b317ae8802bfb7cc5eab0a6a0b8190df2
arun <jaivik.parmar@pardypanda.com>
2025-06-16 11:19:08 +0000

    pm2 3 server ready

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,32 +1,36 @@
+const generateApp = (index)=>({
+  name: `my-server-${index*20 +1}-${index*20 + 20}`,
+  script: './udp-multiple-endpoints.js',
+  
+  // Restart configuration
+  autorestart: true,
+  max_restarts: 10,        // Max restart attempts
+  min_uptime: '10s',       // Min uptime before restart
+  max_memory_restart: '1G', // Restart if memory exceeds 1G
+  
+  // Error handling
+  restart_delay: 4000,     // Delay between restarts (ms)
+  exp_backoff_restart_delay: 100, // Exponential backoff
+  
+//   Logging
+//   log_file: './logs/combined.log',
+//   out_file: './logs/out.log',
+//   error_file: './logs/error.log',
+//   log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+  
+  // Environment
+  env: {
+    NODE_ENV: 'development',
+    PORT: 4001 + index,
+    start : index*20,
+    end : index*20 + 20
+  },
+//   env_production: {
+//     NODE_ENV: 'production',
+//     PORT: 4001
+//   }
+})
+
 module.exports = {
-    apps: [{
-      name: 'my-server',
-      script: './udp-multiple-endpoints.js',
-      
-      // Restart configuration
-      autorestart: true,
-      max_restarts: 10,        // Max restart attempts
-      min_uptime: '10s',       // Min uptime before restart
-      max_memory_restart: '4096M', // Restart if memory exceeds 500MB
-      
-      // Error handling
-      restart_delay: 4000,     // Delay between restarts (ms)
-      exp_backoff_restart_delay: 100, // Exponential backoff
-      
-    //   Logging
-    //   log_file: './logs/combined.log',
-    //   out_file: './logs/out.log',
-    //   error_file: './logs/error.log',
-    //   log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      
-      // Environment
-      env: {
-        NODE_ENV: 'development',
-        PORT: 4001
-      },
-    //   env_production: {
-    //     NODE_ENV: 'production',
-    //     PORT: 4001
-    //   }
-    }]
+    apps: [1,2,3].map((_ , index)=>generateApp(index))
   };
